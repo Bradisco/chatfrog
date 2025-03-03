b84dc2f9c38dbf9dbe697b88358c3d0e8c2560cb
Bradisco <30342643+Bradisco@users.noreply.github.com>
2025-03-03 14:32:08 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+function toggleFAQ() {
+    var faqAnswer = document.getElementById("faq-answer");
+    if (faqAnswer.style.display === "none") {
+        faqAnswer.style.display = "block";
+    } else {
+        faqAnswer.style.display = "none";
+    }
+}
+
